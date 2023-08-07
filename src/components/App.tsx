1ac6e596a1417d96781403dda48402eab37446b1
nathandunn <nathan.dunn.aml@gmail.com>
2023-08-07 11:57:16 -0500

    Changes app structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme } from '@chakra-ui/react';
+import { ChakraProvider, Box, Text, VStack, Grid, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
