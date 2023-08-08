81e3dad1e1c566016664142a39c3635ac1023cdb
nathandunn <nathan.dunn.aml@gmail.com>
2023-08-08 18:38:02 -0500

    Adds transitions

--- src/components/LiftContainer.js
+++ src/components/LiftContainer.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import { Box, Text, WrapItem } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import {
+  Box,
+  Text,
+  WrapItem,
+  Fade,
+  ScaleFade,
+  Slide,
+  SlideFade,
+  Collapse,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { estimate1RM } from '../data';
 import { theme } from '../styles';
 import LiftRecordRow from './LiftRecordRow';
 
 const LiftContainer = ({ liftName, records }) => {
+  const { isOpen, onOpen } = useDisclosure();
+
   const numberOfRecordsDisplayed = 10;
 
   const recordsSorted = records
     .map(record => [...record, estimate1RM(record[1], record[2])])
     .sort((a, b) => (a[3] < b[3] ? -1 : 1))
     .slice(0, numberOfRecordsDisplayed);
+
+  useEffect(() => {
+    setTimeout(() => {
+      onOpen();
+    }, 100);
+  }, [onOpen]);
 
   return (
     <WrapItem w="400px">
@@ -28,7 +46,14 @@ const LiftContainer = ({ liftName, records }) => {
           />
 
           {recordsSorted.map((record, index) => (
-            <LiftRecordRow key={index} row={index + 1} record={record} len={recordsSorted.length} />
+            <SlideFade in={isOpen} offsetY="20px">
+              <LiftRecordRow
+                key={index}
+                row={index + 1}
+                record={record}
+                len={recordsSorted.length}
+              />
+            </SlideFade>
           ))}
         </Box>
       </Box>
